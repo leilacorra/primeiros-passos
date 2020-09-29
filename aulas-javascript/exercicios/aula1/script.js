a15d83705c8487b5a16000e30a5f6ff59c3d2743
Leila Corra <leilacorra@gmail.com>
2020-09-29 22:29:16 +0100

    Aula 1 - exercício 1 - JavaScript

---
+++ aulas-javascript/exercicios/aula1/script.js
@@ -0,0 +1,12 @@
+let idade = prompt("Digite sua idade");
+
+function checarSePodeDirigir(idade){
+    if (idade>18){
+        alert("Já pode dirigir!");
+    
+    }else{
+            alert("Ainda não pode dirigir!");    
+    }
+}
+checarSePodeDirigir(idade);
+
